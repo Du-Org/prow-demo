ced44dcb3fe18717a4fbbec0cc75a824255067fe
djfsmall <149762166+djfsmall@users.noreply.github.com>
2023-11-16 17:48:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello -33- Prow: %d", Random())
+  fmt.Fprintf(w, "Hello -44- Prow: %d", Random())
 }
 
 func Random() int {
